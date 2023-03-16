cc88d2b084d2d8fa50d1cd6c1244697ea815436a
Maxim Gorin <maksim.g@simple.app>
2023-03-16 16:29:30 +0300

    Add fields

--- src/rest/models.rs
+++ src/rest/models.rs
@@ -8,4 +8,24 @@ pub struct Coin {
     pub symbol: String,
     #[serde(rename = "market_cap_rank")]
     pub market_cap_rank: i32,
+    pub market_data: Option<MarketData>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MarketData {
+    pub roi: Option<Roi>,
+    pub market_cap: Option<PriceData>,
+    pub fully_diluted_valuation: Option<PriceData>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PriceData {
+    pub usd: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Roi {
+    pub times: f64,
+    pub currency: String,
+    pub percentage: f64,
 }
